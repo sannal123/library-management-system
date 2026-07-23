8245b84595a35f0228c0b4921c748c6e02d1d005
Sannal Yadav <sannal123.iimt@gmail.com>
2026-07-24 01:06:32 +0530

    Add BookController and book creation form

--- src/main/java/com/sannal/librarymanagement/controller/BookController.java
+++ src/main/java/com/sannal/librarymanagement/controller/BookController.java
@@ -1,21 +1,38 @@
 package com.sannal.librarymanagement.controller;
 
+import org.springframework.ui.Model;
+import com.sannal.librarymanagement.entity.Book;
 import com.sannal.librarymanagement.service.BookService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/books")
-public class HomeController {
+public class BookController {
     private final BookService bookService;
 
     public BookController(BookService bookService){
         this.bookService=bookService;
     }
+    @GetMapping("/new")
+    public  String showBookFrom(Model model){
+        model.addAttribute("book", new Book());
+        return  "book-form";
+    }
 
-    @GetMapping("/")
-    public String home(){
-        return "index";
+    @PostMapping("/save")
+    public  String saveBook(@ModelAttribute Book book){
+        bookService.saveBook(book);
+        return "redirect:/books/list";
+    }
+
+    @GetMapping("/list")
+    public String listBook(Model model){
+        model.addAttribute("books",bookService.getAllBooks());
+        return "book-list";
+
     }
 }
